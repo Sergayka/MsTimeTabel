29a75880e2cfcd4024cedc8bfe0c3a36d69a3911
sergayka <xuverthebest@mail.ru>
2024-12-24 22:42:59 +0300

    add tools+

--- parsingXLS/push_to_db.py
+++ parsingXLS/push_to_db.py
@@ -2,7 +2,7 @@ import os
 import json
 from pymongo import MongoClient
 
-
+# region Push to MongoDB for Groups
 def load_json_to_mongo(json_folder: str, db_name: str):
     # Подключаемся к MongoDB (по умолчанию на localhost:27017)
     client = MongoClient('mongodb://localhost:27017/')
@@ -39,3 +39,54 @@ json_folder = 'schedule_jsons'
 
 # Вызываем функцию для загрузки данных в MongoDB
 load_json_to_mongo(json_folder, 'Schedule')
+
+
+# endregion
+
+
+# region Push to MongoDB for Teachers
+
+import os
+import json
+from pymongo import MongoClient
+
+
+def load_json_to_mongo(json_folder: str, db_name: str):
+    # Подключаемся к MongoDB (по умолчанию на localhost:27017)
+    client = MongoClient('mongodb://localhost:27017/')
+
+    # Создаем/открываем базу данных
+    db = client[db_name]
+
+    # Получаем список всех файлов в папке
+    for filename in os.listdir(json_folder):
+        # Проверяем, что файл имеет расширение .json
+        if filename.endswith('.json'):
+            file_path = os.path.join(json_folder, filename)
+
+            # Извлекаем имя файла без расширения для создания коллекции
+            collection_name = filename.replace('.json', '')
+
+            # Открываем и загружаем JSON данные из файла
+            with open(file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+
+                # Если данные не являются списком, делаем их списком
+                if not isinstance(data, list):
+                    data = [data]
+
+                # Создаем коллекцию с именем файла и добавляем данные
+                collection = db[collection_name]
+                collection.insert_many(data)  # Вставляем все элементы в коллекцию
+
+            print(f"Файл {filename} успешно добавлен как коллекция '{collection_name}'.")
+
+
+# Указываем путь к папке с JSON файлами
+json_folder = 'teachers_schedule_jsons'
+
+# Вызываем функцию для загрузки данных в MongoDB
+load_json_to_mongo(json_folder, 'Teachers')
+
+
+# endregion
